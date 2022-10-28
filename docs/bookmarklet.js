3a06fb84269dac2710ad50f38a92e6a2b5d7dc76
kimizuka <f.kimizuka@gmail.com>
2022-10-29 00:03:39 +0900

    fixed script

--- docs/bookmarklet.js
+++ docs/bookmarklet.js
@@ -1,129 +1,122 @@
-(function (win, doc) {
+(() => {
 
-  "use strict";
+  'use strict';
 
   function PandemicSpan(elm) {
-    var that = this;
-
-    _constructor();
+    _constructor.call(this, elm);
 
     function _constructor() {
-      that.elm       = elm;
-      that.queue     = [_zoomIn, _zoomOut, _zoomIn, _zoomOut, _explode, _vanish];
-      that.achooFlag = false;
-      that.index     = 0;
-      that.elm.style.cssText += PandemicSpan.CONST.DEFAULT_STYLE;
+      this.elm = elm;
+      this.queue = [
+        _zoomIn.bind(this),
+        _zoomOut.bind(this),
+        _zoomIn.bind(this),
+        _zoomOut.bind(this),
+        _explode.bind(this),
+        _vanish.bind(this)
+      ];
+      this.achooFlag = false;
+      this.index = 0;
+      this.elm.style.cssText += PandemicSpan.CONST.DEFAULT_STYLE;
 
-      that.elm.addEventListener("webkitTransitionEnd", _handleTransitionEnd, false);
-      that.elm.addEventListener("mozTransitionEnd", _handleTransitionEnd, false);
-      that.elm.addEventListener("msTransitionEnd", _handleTransitionEnd, false);
-      that.elm.addEventListener("oTransitionEnd", _handleTransitionEnd, false);
-      that.elm.addEventListener("transitionend", _handleTransitionEnd, false);
+      this.elm.addEventListener('webkitTransitionEnd', _handleTransitionEnd.bind(this), false);
+      this.elm.addEventListener('mozTransitionEnd', _handleTransitionEnd.bind(this), false);
+      this.elm.addEventListener('msTransitionEnd', _handleTransitionEnd.bind(this), false);
+      this.elm.addEventListener('oTransitionEnd', _handleTransitionEnd.bind(this), false);
+      this.elm.addEventListener('transitionend', _handleTransitionEnd.bind(this), false);
     }
 
     function _zoomIn() {
-      that._zoomIn();
+      this._zoomIn();
     }
 
     function _zoomOut() {
-      that._zoomOut();
+      this._zoomOut();
     }
 
     function _explode() {
-      that._explode();
+      this._explode();
     }
 
     function _vanish() {
-      that._vanish();
+      this._vanish();
     }
 
     function _handleTransitionEnd(evt) {
-      that._handleTransitionEnd(evt);
+      this._handleTransitionEnd(evt);
     }
   }
 
   PandemicSpan.CONST = {
     DEFAULT_STYLE : [
-      ";",
-      "display: inline-block;",
-      "-webkit-transition: all 1s ease;",
-      "-moz-transition: all 1s ease;",
-      "-ms-transition: all 1s ease;",
-      "-o-transition: all 1s ease;",
-      "transition: all 1s ease;",
-      "-webkit-transform: rotateZ(0deg);",
-      "-moz-transform: rotateZ(0deg);",
-      "-ms-transform: rotateZ(0deg);",
-      "-o-transform: rotateZ(0deg);",
-      "transform: rotateZ(0deg);",
-      "font-size: 100%;",
-      "opacity: 1;"
-    ].join(""),
-    KLASS : "k-virus"
+      ';',
+      'display: inline-block;',
+      '-webkit-transition: all 1s ease;',
+      '-moz-transition: all 1s ease;',
+      '-ms-transition: all 1s ease;',
+      '-o-transition: all 1s ease;',
+      'transition: all 1s ease;',
+      '-webkit-transform: rotateZ(0deg);',
+      '-moz-transform: rotateZ(0deg);',
+      '-ms-transform: rotateZ(0deg);',
+      '-o-transform: rotateZ(0deg);',
+      'transform: rotateZ(0deg);',
+      'font-size: 100%;',
+      'opacity: 1;'
+    ].join(''),
+    KLASS : 'k-virus'
   };
 
   PandemicSpan.prototype._handleTransitionEnd = function(evt) {
-    var that = this;
+    if (!!this.achooFlag) {
+      ++this.index;
 
-    if (!!that.achooFlag) {
-      ++that.index;
-      if (that.index < that.queue.length) {
-        that.queue[that.index]();
+      if (this.index < this.queue.length) {
+        this.queue[this.index]();
       }
     }
   };
 
   PandemicSpan.prototype._zoomIn = function() {
-    var that = this;
-
-    that.elm.style["font-size"] = "200%";
+    this.elm.style['font-size'] = '200%';
   };
 
   PandemicSpan.prototype._zoomOut = function() {
-    var that = this;
-
-    that.elm.style["font-size"] = "50%";
+    this.elm.style['font-size'] = '50%';
   };
 
   PandemicSpan.prototype._explode = function() {
-    var that = this;
-
-    that.elm.style["-webkit-transform"] = "rotateZ(360deg)";
-    that.elm.style["-moz-transform"] = "rotateZ(360deg)";
-    that.elm.style["-ms-transform"] = "rotateZ(360deg)";
-    that.elm.style["-o-transform"] = "rotateZ(360deg)";
-    that.elm.style["transform"] = "rotateZ(360deg)";
-    that.elm.style["font-size"] = "5000%";
-    that.elm.style["opacity"] = "0";
+    this.elm.style['-webkit-transform'] = 'rotateZ(360deg)';
+    this.elm.style['-moz-transform'] = 'rotateZ(360deg)';
+    this.elm.style['-ms-transform'] = 'rotateZ(360deg)';
+    this.elm.style['-o-transform'] = 'rotateZ(360deg)';
+    this.elm.style['transform'] = 'rotateZ(360deg)';
+    this.elm.style['font-size'] = '5000%';
+    this.elm.style['opacity'] = '0';
   };
 
   PandemicSpan.prototype._vanish = function() {
-    var that = this;
-
-    that.elm.style.display = "none";
-
-    that.achooFlag = false;
-    that.index = 0;
+    this.elm.style.display = 'none';
+    this.achooFlag = false;
+    this.index = 0;
   };
 
   PandemicSpan.prototype.achoo = function() {
-    var that = this;
-
-    that.achooFlag = true;
-    that.queue[that.index]();
+    this.achooFlag = true;
+    this.queue[this.index]();
   };
 
-  var index = 0,
-      sickSpanList = [];
-  
-  [].slice.call(doc.querySelectorAll("." + PandemicSpan.CONST.KLASS)).forEach(function(dom) {
-    dom.style.cssText = "";
+  let index = 0;
+  let sickSpanList = [];
+
+  [].slice.call(document.querySelectorAll(`.${ PandemicSpan.CONST.KLASS }`)).forEach((dom) => {
+    dom.style.cssText = '';
     dom.classList.remove(PandemicSpan.CONST.KLASS);
   });
 
-  examination(doc.body);
+  examination(document.body);
 
-  sickSpanList = [].slice.call(doc.querySelectorAll("." + PandemicSpan.CONST.KLASS)).map(function(sickSpan) {
+  sickSpanList = [].slice.call(document.querySelectorAll(`.${ PandemicSpan.CONST.KLASS }`)).map((sickSpan) => {
     return new PandemicSpan(sickSpan);
   });
 
@@ -133,20 +126,19 @@
       setTimeout(catchCold, index);
     }
   }, 500);
-    
-  function examination(dom) {
-      [].slice.call(dom.childNodes).forEach(function(child) {
-          if (child.nodeType === 3) {
-              child.textContent = child.textContent.replace(/(か|ぜ|カ|ゼ|風|邪)/g, "<<span<<$1>>span>>");
-          } else {
-              examination(child);
-          }
-      });
-      
-      if (dom.innerHTML) {
-          dom.innerHTML = dom.innerHTML.replace(/&lt;&lt;span&lt;&lt;/g, "<span class='" + PandemicSpan.CONST.KLASS + "'>");
-          dom.innerHTML = dom.innerHTML.replace(/&gt;&gt;span&gt;&gt;/g, "</span>");
-      }
-  }
 
-})(this, document);
+  function examination(dom) {
+    [].slice.call(dom.childNodes).forEach((child) => {
+      if (child.nodeType === 3) {
+        child.textContent = child.textContent.replace(/(か|ぜ|カ|ゼ|風|邪)/g, '<<span<<$1>>span>>');
+      } else {
+        examination(child);
+      }
+    });
+
+    if (dom.innerHTML) {
+      dom.innerHTML = dom.innerHTML.replace(/&lt;&lt;span&lt;&lt;/g, `<span class="${ PandemicSpan.CONST.KLASS }">`);
+      dom.innerHTML = dom.innerHTML.replace(/&gt;&gt;span&gt;&gt;/g, '</span>');
+    }
+  }
+})();
